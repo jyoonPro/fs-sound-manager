b6c908981de9d7daee0bd7961507f08955f85a38
Joseph Yoon <joseph@jyoon.pro>
2023-05-24 23:39:09 +0900

    Remove unnecessary tokio handler

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -9,14 +9,12 @@ use serde::{Deserialize, Serialize};
 use std::net::{Ipv4Addr, SocketAddr};
 
 pub struct AudioServer {
-    tokio_handle: Option<tokio::task::JoinHandle<()>>,
     axum_handle: Option<Handle>,
 }
 
 impl AudioServer {
     pub fn new() -> Self {
         Self {
-            tokio_handle: None,
             axum_handle: None,
         }
     }
@@ -30,22 +28,18 @@ impl AudioServer {
         self.axum_handle = Some(handle.clone());
 
         let addr = SocketAddr::from(([127, 0, 0, 1], port));
-        self.tokio_handle = Some(tokio::spawn(async move {
+        tokio::spawn(async move {
             axum_server::bind(addr)
                 .handle(handle)
                 .serve(app.into_make_service())
                 .await
                 .unwrap();
-        }));
+        });
     }
 
     pub fn stop(&mut self) {
         if let Some(axum_handle) = self.axum_handle.take() {
             axum_handle.shutdown();
-
-            if let Some(tokio_handle) = self.tokio_handle.take() {
-                tokio_handle.abort();
-            }
         }
     }
 }
